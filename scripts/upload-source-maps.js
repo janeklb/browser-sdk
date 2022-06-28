17fe1b01d4659ffce7b931d8c47a83f7737dd28f
Bastien Caudan <bastien.caudan@gmail.com>
2022-06-28 15:47:19 +0200

    fix renaming files

--- scripts/upload-source-maps.js
+++ scripts/upload-source-maps.js
@@ -12,9 +12,7 @@ const { SDK_VERSION } = require('./build-env')
 const sites = process.argv[2].split(',')
 const suffix = process.argv[3]
 
-async function uploadSourceMaps(site, apiKey, packageName) {
-  const bundleFolder = `packages/${packageName}/bundle`
-
+async function renameFiles(bundleFolder, packageName) {
   // The datadog-ci CLI is taking a directory as an argument. It will scan every source map files in
   // it and upload those along with the minified bundle. The file names must match the one from the
   // CDN, thus we need to rename the bundles with the right suffix.
@@ -23,7 +21,9 @@ async function uploadSourceMaps(site, apiKey, packageName) {
     const suffixedFilePath = `${bundleFolder}/datadog-${packageName}-${suffix}.${ext}`
     await executeCommand(`mv ${filePath} ${suffixedFilePath}`)
   }
+}
 
+async function uploadSourceMaps(site, apiKey, packageName, bundleFolder) {
   const output = await executeCommand(
     `
     datadog-ci sourcemaps upload ${bundleFolder} \
@@ -42,15 +42,17 @@ async function uploadSourceMaps(site, apiKey, packageName) {
 }
 
 async function main() {
-  for (const site of sites) {
-    const normalizedSite = site.replaceAll('.', '-')
-    const apiKey = await getSecretKey(`ci.browser-sdk.source-maps.${normalizedSite}.ci_api_key`)
+  for (const packageName of ['logs', 'rum', 'rum-slim']) {
+    const bundleFolder = `packages/${packageName}/bundle`
+    await renameFiles(bundleFolder, packageName, bundleFolder)
+    for (const site of sites) {
+      const normalizedSite = site.replaceAll('.', '-')
+      const apiKey = await getSecretKey(`ci.browser-sdk.source-maps.${normalizedSite}.ci_api_key`)
 
-    for (const packageName of ['logs', 'rum', 'rum-slim']) {
       await uploadSourceMaps(site, apiKey, packageName)
     }
-    printLog(`Source maps uploaded for ${site}.`)
   }
+  printLog('Source maps upload done.')
 }
 
 main().catch(logAndExit)
